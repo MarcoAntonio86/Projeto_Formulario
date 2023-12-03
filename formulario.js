6e4a51e2dbf953c35f7bf5ce826f7f72abea7eb4
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2023-12-03 16:59:40 -0300

    Initial commit

---
+++ formulario.js
@@ -0,0 +1,2 @@
+var nome = prompt("Qual seu nome?");
+alert("Olá " + nome + ", por favor responda o questionário abaixo, é uma pesquisa sobre a visão, que as pessoas tem, sobre Tecnologia da Informação (TI).");
